b1118ff52a3098ec2320c0a77c757e6c2d10c973
r4yyygit config --global user.email raymundturallo@gmail.com <raymundturallo@gmail.com>
2026-02-23 16:29:01 +0800

    updated. header errors in winser2008ins file

--- Includes/script.js
+++ Includes/script.js
@@ -1,4 +1,4 @@
-fetch('header.html')
+fetch('/header.html')
   .then(res => res.text())
   .then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
@@ -6,17 +6,13 @@ fetch('header.html')
     // Runs after header is loaded
     document.querySelectorAll('.nav-items > li').forEach(item => {
       const link = item.querySelector('a');
-
       link.addEventListener('click', (e) => {
         const hasDropdown = item.querySelector('ul');
         if (!hasDropdown) return;
-
         e.preventDefault();
         e.stopPropagation();
         const isOpen = item.classList.contains('open');
-
         document.querySelectorAll('.nav-items > li').forEach(i => i.classList.remove('open'));
-
         if (!isOpen) item.classList.add('open');
       });
     });
